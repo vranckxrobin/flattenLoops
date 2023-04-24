f23cc8504d688988e265ee58c3c7e63468883ef4
vranckxrobin <vranckxrobin@hotmail.com>
2023-04-24 21:15:13 +0200

    refactoring optimizations.py

--- optimizations.py
+++ optimizations.py
@@ -1,31 +1,18 @@
 from pycparser import c_ast
 
 
+def changeCaseToIf(cond, case):
+    caseBlocks = [tempBlock for tempBlock in case.stmts if type(tempBlock) != c_ast.Break]
+    return c_ast.If(c_ast.BinaryOp('==', cond, case.expr, None), c_ast.Compound(caseBlocks), None, None)
+
+
 def replaceSwitch(ast, function):
-    newAst = c_ast.FileAST([], ast.coord)
-    for func in ast.ext:
-        name = func.decl.name
-        if name == function:
-            functionBlocks = []
-            for block in func.body.block_items:
-                if type(block) == c_ast.While:
-                    whileItems = []
-                    for whileBlock in block.stmt.block_items:
-                        if type(whileBlock) == c_ast.Switch:
-                            cond = whileBlock.cond
-                            for case in whileBlock.stmt.block_items:
-                                caseBlocks = [
-                                    tempBlock
-                                    for tempBlock in case.stmts
-                                    if type(tempBlock) != c_ast.Break
-                                ]
-                                whileItems.append(
-                                    c_ast.If(c_ast.BinaryOp('==', cond, case.expr, None), c_ast.Compound(caseBlocks),
-                                             None, None))
-                        else:
-                            whileItems.append(whileBlock)
-                    functionBlocks.append(c_ast.While(block.cond, c_ast.Compound(whileItems)))
-                else:
-                    functionBlocks.append(block)
-            newAst.ext.append(c_ast.FuncDef(func.decl, func.param_decls, c_ast.Compound(functionBlocks), None))
-    return newAst
+    func = next(func for func in ast.ext if func.decl.name == function)
+    whileIndex = next((i for i, block in enumerate(func.body.block_items) if isinstance(block, c_ast.While)), 0)
+    whileLoop = func.body.block_items[whileIndex]
+    switch = next(block for block in whileLoop.stmt.block_items if isinstance(block, c_ast.Switch))
+    ifStatements = [changeCaseToIf(switch.cond, case) for case in switch.stmt.block_items]
+
+    newFuncBody = func.body.block_items[:whileIndex] + [c_ast.While(whileLoop.cond, c_ast.Compound(ifStatements))]
+
+    return c_ast.FileAST([c_ast.FuncDef(func.decl, func.param_decls, c_ast.Compound(newFuncBody), None)])
