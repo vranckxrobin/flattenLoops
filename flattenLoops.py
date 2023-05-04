e62e9e743c491b259f2ce644c71e43ee8382718d
vranckxrobin <vranckxrobin@hotmail.com>
2023-05-04 10:06:01 +0200

    refactoring of flattenLoops.py

--- flattenLoops.py
+++ flattenLoops.py
@@ -6,14 +6,14 @@ import copy
 sys.path.extend(['.', '..'])
 
 
-class FlattenPart:
-    def __init__(self, block, currentCaseNumber, returnToCase):
+class FlattenStmt:
+    def __init__(self, stmt, currentCaseNumber, returnToCase):
         self.currentCaseNumber = currentCaseNumber
         self.returnToCase = returnToCase  # TODO Note if zero no returnStatement
         self.cases = []  # cases to represent the flattened part
         self.caseNumber = currentCaseNumber
         self.callsFunctionNotInFile = False
-        self.flattenCaseByType(block)
+        self.flattenCaseByType(stmt)
 
     def getCurrentCaseNumber(self):
         return self.currentCaseNumber
@@ -21,43 +21,44 @@ class FlattenPart:
     def getCases(self):
         return self.cases
 
-    def transformWhileBodyToCases(self, block, returnToWhile):
-        lengthBlock_items = len(block.stmt.block_items) - 1
-        for pos, tempBlock in enumerate(block.stmt.block_items):
+    def transformWhileBodyToCases(self, stmt, returnToWhile):
+        lengthBlock_items = len(stmt.stmt.block_items) - 1
+        for pos, tempBlock in enumerate(stmt.stmt.block_items):
             returnToCase = returnToWhile if pos == lengthBlock_items else 0  # state to which case to return to 0 is go to next case
-            flattenPart = FlattenPart(tempBlock, self.currentCaseNumber,
+            flattenStmt = FlattenStmt(tempBlock, self.currentCaseNumber,
                                       returnToCase)  # actual flattening of part of the while loop
-            self.cases.extend(flattenPart.getCases())  # add cases to the new functions switch body
+            self.cases.extend(flattenStmt.getCases())  # add cases to the new functions switch body
             # set the case number to the old case number + the cases created by flattening part of the while loop body
-            self.currentCaseNumber = flattenPart.getCurrentCaseNumber()
+            self.currentCaseNumber = flattenStmt.getCurrentCaseNumber()
 
-    def transformWhileConditionToIf(self, block, caseNumber, returnToCase, nextInstruction):
+    def transformWhileConditionToIf(self, stmt, caseNumber, returnToCase, nextInstruction):
         falseCase = [c_ast.Assignment("=", c_ast.ID("programStep"),
                                       c_ast.Constant("int", str(returnToCase)))]
 
-        case = [c_ast.If(block.cond, c_ast.Compound([c_ast.Assignment("=", c_ast.ID("programStep"),
-                                                                      c_ast.Constant("int", str(nextInstruction)))]),
+        case = [c_ast.If(stmt.cond, c_ast.Compound([c_ast.Assignment("=", c_ast.ID("programStep"),
+                                                                     c_ast.Constant("int", str(nextInstruction)))]),
                          c_ast.Compound(falseCase)),
                 c_ast.Break()]
         self.cases.append(
             c_ast.Case(c_ast.Constant("int", str(caseNumber)), case))
 
-    def handleWhileCase(self, block):
+    def handleWhileCase(self, stmt):
         self.currentCaseNumber = self.currentCaseNumber + 1  # add case because first case is the while's if statement
         nextInstruction = self.currentCaseNumber
-        self.transformWhileBodyToCases(block, self.caseNumber)
+        self.transformWhileBodyToCases(stmt, self.caseNumber)
         returnToCase = self.returnToCase if self.returnToCase != 0 else self.currentCaseNumber  # Sets to which case to return to
-        self.transformWhileConditionToIf(block, self.caseNumber, returnToCase, nextInstruction)
+        self.transformWhileConditionToIf(stmt, self.caseNumber, returnToCase, nextInstruction)
 
-    def handleDoWhileCase(self, block):
+    def handleDoWhileCase(self, stmt):
         nextInstruction = self.currentCaseNumber
-        self.transformWhileBodyToCases(block, 0)
+        self.transformWhileBodyToCases(stmt, 0)
         returnToCase = self.returnToCase if self.returnToCase != 0 else self.currentCaseNumber + 1  # Sets to which case to return to
-        self.transformWhileConditionToIf(block, self.currentCaseNumber, returnToCase, nextInstruction)
+        self.transformWhileConditionToIf(stmt, self.currentCaseNumber, returnToCase, nextInstruction)
         self.currentCaseNumber = self.currentCaseNumber + 1  # add case because last case is the while's if statement
 
         # flattens if statements and calls this class when part of the if statement cases should be flattened
-    def handleIfCase(self, block):
+
+    def handleIfCase(self, stmt):
         saveSelf = copy.deepcopy(self)  # save state
 
         returnToCase = self.returnToCase
@@ -68,8 +69,8 @@ class FlattenPart:
         # else case should point to
         if returnToCase == 0:
             self.currentCaseNumber += 1
-            self.handleIfBody(block, 'iftrue')
-            self.handleIfBody(block, 'iffalse')
+            self.handleIfBody(stmt, 'iftrue')
+            self.handleIfBody(stmt, 'iffalse')
             returnToCase = self.returnToCase if self.returnToCase != 0 else self.currentCaseNumber
 
             # reset the previous save state except for the returnToCase
@@ -81,32 +82,33 @@ class FlattenPart:
 
         self.callsFunctionNotInFile = False  # Used to add the programSteps in the else case and remove the redunt ones under the if else statement.
         self.currentCaseNumber += 1  # add 1 for the case of the if statement itself
-        trueCase = self.handleIfBody(block, 'iftrue')  # flattens true case
+        trueCase = self.handleIfBody(stmt, 'iftrue')  # flattens true case
         self.returnToCase = saveSelf.returnToCase  # reset saved return case
-        falseCase = self.handleIfBody(block, 'iffalse')  # flattens false case
+        falseCase = self.handleIfBody(stmt, 'iffalse')  # flattens false case
 
         # add programStep in the else case if the else is empty
         if falseCase is None and not self.callsFunctionNotInFile:
             falseCase = c_ast.Compound([c_ast.Assignment("=", c_ast.ID("programStep"),
                                                          c_ast.Constant("int", str(returnToCase)))])
 
-        case = [c_ast.If(block.cond, trueCase, falseCase)]
+        case = [c_ast.If(stmt.cond, trueCase, falseCase)]
         if self.callsFunctionNotInFile:
             case.append(c_ast.Assignment("=", c_ast.ID("programStep"),
                                          c_ast.Constant("int",
                                                         str(returnToCase))))  # point to what is outside the if statement (returnToCase)
         case.append(c_ast.Break())
         self.cases.append(
-            c_ast.Case(c_ast.Constant("int", str(self.caseNumber)), case))  # program step of this case (self.caseNumber)
+            c_ast.Case(c_ast.Constant("int", str(self.caseNumber)),
+                       case))  # program step of this case (self.caseNumber)
 
     # For cases that don't need to be flattened
     # Create a case with the block inside
-    def handleDefault(self, block):
+    def handleDefault(self, stmt):
         self.currentCaseNumber += 1
         returnToCase = (
             self.returnToCase if self.returnToCase != 0 else self.currentCaseNumber
         )
-        temp = [block, c_ast.Assignment(
+        temp = [stmt, c_ast.Assignment(
             "=",
             c_ast.ID("programStep"),
             c_ast.Constant("int", str(returnToCase))
@@ -114,16 +116,16 @@ class FlattenPart:
         self.cases.append(
             c_ast.Case(c_ast.Constant("int", str(self.caseNumber)), temp))
 
-    def flattenCaseByType(self, block):
-        match type(block):  # TODO add switch
+    def flattenCaseByType(self, stmt):
+        match type(stmt):  # TODO add switch
             case c_ast.While:
-                self.handleWhileCase(block)
+                self.handleWhileCase(stmt)
             case c_ast.DoWhile:
-                self.handleDoWhileCase(block)
+                self.handleDoWhileCase(stmt)
             case c_ast.If:
-                self.handleIfCase(block)
+                self.handleIfCase(stmt)
             case _:  #
-                self.handleDefault(block)
+                self.handleDefault(stmt)
 
     # Determines which cases need to be flattened.
     def needsFlattening(self, param):
@@ -149,26 +151,26 @@ class FlattenPart:
         lengthBlock_items = len(ifcase.block_items.block_items) - 1
         for pos, tempBlock in enumerate(ifcase.block_items.block_items):
             if self.needsFlattening(type(tempBlock)):  # Checks if the current statement needs to be flattened.
-                if creatingCase: #If there was a case that was being made, create it first.
+                if creatingCase:  # If there was a case that was being made, create it first.
                     creatingCase = False
                     self.createCase(temp, 0)
                     temp = []
-                #Flatten the current statement
+                # Flatten the current statement
                 returnToCase = self.returnToCase if pos == lengthBlock_items else 0
-                flattenPart = FlattenPart(tempBlock, self.currentCaseNumber,
+                flattenStmt = FlattenStmt(tempBlock, self.currentCaseNumber,
                                           returnToCase)
-                self.cases.extend(flattenPart.getCases()) #Add the flattened case to these cases
+                self.cases.extend(flattenStmt.getCases())  # Add the flattened case to these cases
                 # Set the case number to the old case number + the cases created by flattening part of the while loop body.
-                self.currentCaseNumber = flattenPart.getCurrentCaseNumber()
+                self.currentCaseNumber = flattenStmt.getCurrentCaseNumber()
             else:
                 creatingCase = True
                 temp.append(tempBlock)
-        if creatingCase: # If there was a case being made create it
+        if creatingCase:  # If there was a case being made create it
             self.createCase(temp, self.returnToCase)
 
     # Create cases for if statements case (true case or false case)
-    def handleIfBody(self, block, ifCaseString):
-        ifcase = getattr(block, ifCaseString)
+    def handleIfBody(self, stmt, ifCaseString):
+        ifcase = getattr(stmt, ifCaseString)
         if ifCaseString == 'iffalse' and ifcase is None:
             return None
         jumpToCase = self.currentCaseNumber
@@ -180,10 +182,10 @@ class FlattenPart:
         elif isinstance(ifcase, c_ast.If):
             trueCase = c_ast.Compound([c_ast.Assignment("=", c_ast.ID("programStep"),
                                                         c_ast.Constant("int", str(self.currentCaseNumber)))])
-            flattenPart = FlattenPart(ifcase, self.currentCaseNumber,
+            flattenStmt = FlattenStmt(ifcase, self.currentCaseNumber,
                                       self.returnToCase)  # Note that this happens only in the false case of if
-            self.cases.extend(flattenPart.getCases())
-            self.currentCaseNumber = flattenPart.getCurrentCaseNumber()
+            self.cases.extend(flattenStmt.getCases())
+            self.currentCaseNumber = flattenStmt.getCurrentCaseNumber()
 
         else:
             self.callsFunctionNotInFile = (
@@ -193,7 +195,7 @@ class FlattenPart:
 
 
 class FlattenFunc:
-    def __init__(self, ast):
+    def __init__(self, funcBody):
         # Adds 2 variables to the program to control the flow of the program
         self.newBlockItems = [
             c_ast.Decl("run", None, None, None, None,
@@ -204,11 +206,11 @@ class FlattenFunc:
                        c_ast.Constant("int", "1"), None)
         ]
 
-        ast = self.appendDeclerations(ast)
+        funcBody = self.appendDeclerations(funcBody)
         self.caseCount = 1
         self.switchBody = []
 
-        self.createSwitchBody(ast)  # Create the cases for the switch
+        self.createSwitchBody(funcBody)  # Create the cases for the switch
         self.createLastCaseOfSwitchBody()
 
         # Create the switch statement
@@ -218,11 +220,11 @@ class FlattenFunc:
         self.newBlockItems.append(c_ast.While(c_ast.ID("run"), c_ast.Compound(switch)))
 
     # Create cases for the switch body
-    def createSwitchBody(self, ast):
-        for block in ast:
-            flattenPart = FlattenPart(block, self.caseCount, 0)
-            self.switchBody.extend(flattenPart.getCases())
-            self.caseCount = flattenPart.getCurrentCaseNumber()
+    def createSwitchBody(self, funcBody):
+        for stmt in funcBody:
+            flattenStmt = FlattenStmt(stmt, self.caseCount, 0)
+            self.switchBody.extend(flattenStmt.getCases())
+            self.caseCount = flattenStmt.getCurrentCaseNumber()
         self.sortSwitchBody()
 
     # Create the last case for the switch body, the termination of the loop.
@@ -235,21 +237,23 @@ class FlattenFunc:
         return self.newBlockItems
 
     # Adds the declarations to the newBlockItems variable (which will be the new function)
-    def appendDeclerations(self, ast):
-        for count, block in enumerate(ast.block_items):
-            if c_ast.Decl != type(block):
-                return ast.block_items[count:]
-            self.newBlockItems.append(block)
+    def appendDeclerations(self, funcBody):
+        for count, stmt in enumerate(funcBody.block_items):
+            if not isinstance(stmt, c_ast.Decl):
+                return funcBody.block_items[count:]
+            self.newBlockItems.append(stmt)
 
     # Sort switch by programStep
     def sortSwitchBody(self):
         self.switchBody.sort(key=lambda x: int(x.expr.value))
+
 
 # Flattens a function
 def flattenFunction(func):
     flattenFunc = FlattenFunc(func.body)
     newBody = c_ast.Compound(flattenFunc.getFlattenedLoop())
     return c_ast.FuncDef(func.decl, func.param_decls, newBody)
+
 
 # Flattens all the functions
 def flattenFile(ast):
